5ea491ac8ab96d66feefa3521d5eec4b339cbb75
travis-russell <russell.travis@yahoo.com>
2019-03-01 11:30:58 -0600

    combined user tables

--- back-end/data/migrations/properties.js
+++ back-end/data/migrations/properties.js
@@ -1,9 +1,9 @@
 exports.up = function(knex, Promise) {
 	return knex.schema.createTable('properties', (table) => {
 		table.increments();
-		table.integer('owner').notNullable().references('id').inTable('owners');
-		table.integer('tenant1').noteNullable().references('id').inTable('tenants');
-		table.integer('tenant2').references('id').inTable('tenants');
+		table.integer('owner').notNullable().references('id').inTable('users');
+		table.integer('tenant1').noteNullable().references('id').inTable('users');
+		table.integer('tenant2').references('id').inTable('users');
 		table.string('address').noteNullable();
 		table.integer('maxOccupants').notNullable().defaultTo(2);
 		table.integer('sqft').notNullable();
