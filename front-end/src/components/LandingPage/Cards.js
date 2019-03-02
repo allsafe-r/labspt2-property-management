9fc013c86fd784c4d1806f8faa3c353de99c92b6
unknown <kyleteeter12@gmail.com>
2019-03-02 14:03:37 -0600

    Made Nav Bar and three cards for showing benefits of app.

---
+++ front-end/src/components/LandingPage/Cards.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+
+
+
+const styles = {
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
+
+function Cards(props) {
+  const { classes } = props;
+
+  return (
+      <div class="flex-cards">
+    <Card className={classes.card}>
+      <CardContent>
+      <CardMedia
+          className={classes.media}
+          image="./buy.png"
+          title="Paella dish"
+        />
+        <div class="card-title">
+            Easy to Use
+        </div>
+      </CardContent>
+    </Card>
+
+    <Card className={classes.card}>
+        
+        <CardContent>
+        <div class="card-title">
+            Easy to Use
+        </div>
+      </CardContent>
+    </Card>
+
+    
+    <Card className={classes.card}>
+        <CardContent>
+        <div class="card-title">
+            Easy to Use
+        </div>
+      </CardContent>
+    </Card>
+
+    </div>
+  );
+}
+
+Cards.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Cards);
