7e3bac290b63b245e6e72b0c1b8ddb65c747d264
Lee Len Formento <leebatef@gmail.com>
2019-03-04 20:03:24 -0500

    properties

--- back-end/data/helper/propertiesModal.js
+++ back-end/data/helper/propertiesModal.js
@@ -10,30 +10,30 @@ module.exports = {
 	editProperty
 };
 
-function getPropertiesByTen1(id) {
+function getPropertiesByTen1(houseId) {
 	return db('property').then((res) => res.filter((prop) => prop.tenant1 == id));
 }
 
-function getPropertiesByTen2(id) {
+function getPropertiesByTen2(houseId) {
 	return db('properties').then((res) => res.filter((prop) => prop.tenant2 == id));
 }
 
-function getPropertiesByOwner(id) {
+function getPropertiesByOwner(houseId) {
 	return db('properties').then((res) => res.filter((prop) => prop.owner == id));
 }
 
-function getProperty(id) {
-	return db('properties').where({ id }).first();
+function getProperty(houseId) {
+	return db('properties').where({ houseId }).first();
 }
 
 function createProperty(property) {
 	return db('properties').insert(property, 'id');
 }
 
-function deleteProperty(id) {
+function deleteProperty(houseId) {
 	return db('properties').where({ id }).del();
 }
 
-function editProperty(id, property) {
+function editProperty(houseId, property) {
 	return db('properties').where({ id }).update(property, 'id');
 }
