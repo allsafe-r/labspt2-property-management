624eca93dd78d3aabf0d3fe1d5814dc697412695
Lee Len Formento <leebatef@gmail.com>
2019-03-04 20:10:21 -0500

    fixed users endpoints to correspond to travis code

--- back-end/data/migrations/users.js
+++ back-end/data/migrations/users.js
@@ -1,19 +1,15 @@
 exports.up = function(knex, Promise) {
 	return knex.schema.createTable('users', (table) => {
-		table.increments('userId');
-		table.string('uId');
-		table.string('firstName').notNullable();
-		table.string('middleName').notNullable();
-		table.string('lastName').notNullable();
-		table.boolean('is_admin').notNullable();
-		table.string('email').notNullable().unique();
-		table.integer('phone').notNullable();
+		table.increments();
+		table.string('name').notNullable();
+		table.boolean('is_admin').notNullable().defaultTo(false);
+		table.string('email').notNullable();
+		table.string('phone').notNullable();
 		table.string('displayName').notNullable();
-		table.boolean('forgetPassword').notNullable.defaultTo(false);
 		table.boolean('emailSubscribe').notNullable.defaultTo(false);
 		table.boolean('textSubscribe').notNullable.defaultTo(false);
-		// table.integer('residence_id').references('id').inTable('properties');
-		// table.text('application');
+		table.integer('residence_id').references('id').inTable('properties');
+		table.text('application');
 	});
 };
 exports.down = function(knex, Promise) {
