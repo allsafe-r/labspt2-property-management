de1763508c63bfc8b452f560bd1b87036cf5d7f6
Matthew Meitl <darksamus6@gmail.com>
2019-03-05 22:33:49 -0600

    added intial work order component files

---
+++ front-end/src/components/workorderCard.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+
+
+const workorderCard = () => {
+    return (
+        <h1>Hi</h1>
+    )
+}
+
+export default workorderCard;
---
+++ front-end/src/components/workorderList.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import workorderCard from "./workorderCard";
+
+const workorderList = () => {
+    return (
+        <h1>Hi</h1>
+    )
+}
+
+export default workorderList;
