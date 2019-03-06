a4c7d4a3d6175a2935f8b9148a63c3d84a10425f
travis-russell <russell.travis@yahoo.com>
2019-03-06 15:54:36 -0600

    fixed propModal inconsistencies

--- back-end/data/helper/propertiesModal.js
+++ back-end/data/helper/propertiesModal.js
@@ -10,15 +10,15 @@ module.exports = {
 	editProperty
 };
 
-function getPropertiesByTen1(houseId) {
+function getPropertiesByTen1(id) {
 	return db('property').then((res) => res.filter((prop) => prop.tenant1 == id));
 }
 
-function getPropertiesByTen2(houseId) {
+function getPropertiesByTen2(id) {
 	return db('properties').then((res) => res.filter((prop) => prop.tenant2 == id));
 }
 
-function getPropertiesByOwner(houseId) {
+function getPropertiesByOwner(id) {
 	return db('properties').then((res) => res.filter((prop) => prop.owner == id));
 }
 
@@ -35,5 +35,5 @@ function deleteProperty(houseId) {
 }
 
 function editProperty(houseId, property) {
-	return db('properties').where({ id }).update(property, 'id');
+	return db('properties').where({ houseId }).update(property, 'houseId');
 }
