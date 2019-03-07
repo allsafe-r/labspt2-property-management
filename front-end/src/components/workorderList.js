71806f3b5bf1b633c7b99a7bc4b095452426c94e
Matthew Meitl <darksamus6@gmail.com>
2019-03-06 19:32:29 -0600

    added placeholder axios request

--- front-end/src/components/workorderList.js
+++ front-end/src/components/workorderList.js
@@ -1,16 +1,38 @@
 import React, { Component } from "react";
-
+import axios from 'axios';
 import workorderCard from "./workorderCard";
 
-const workorderList = (props) => {
+export default class workorderList extends Component  {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            workorders: []
+        };
+    }
+
+    componentDidMount(){
+        axios
+        .get('/api/workorders')
+        .then(response => {
+            this.setState(() => ({workorders: response.data}));
+        })
+        .catch(error => {
+            console.error('Server Error', error);
+        });
+    }
+
+
+
+render(){
     return (
         <div className="workorderlist">
         {this.props.workorders.map(work => (
             <workorderCard key={work.id} work={work} />
         ))}
         </div>
-      </div>
+    
     )
 }
 
-export default workorderList;
+}
