f0a438109cb14360d357f60f7dc3b85bd3582e03
Lee Len Formento <leebatef@gmail.com>
2019-03-06 22:04:39 -0500

    fixed endpoints for consistency

--- back-end/api/properties.js
+++ back-end/api/properties.js
@@ -1,90 +1,90 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../knexfile.js');
+const db = require('../data/helper/propertiesModal');
 
 // Get all properties
-router.get('/properties', (req, res) => {
-    db('properties')
-      .then(property => {
-        res.status(200).json(property);
-      })
-      .catch(err => {
-          res.status(500).json({ error: `${err}` })}
-      );
-  });
+router.get('/', (req, res) => {
+	db.getProperties().then((properties) => res.status(200).json(properties)).catch((err) => {
+		res.status(500).json({ error: `${err}` });
+	});
+});
 
 // Get a property
-router.get('/properties/:houseId', (req, res) => {
-    const { id } = req.params;
-    db('house_properties')
-      .where('properties.houseId', id)
-      .first()
-      .then(property => {
-        if (!property) {
-          res.status(401).json({ error: 'The specified property does not exist.' });
-          return;
-        } else res.status(200).json(property);
-      })
-      .catch(err => res.status(500).json(err));
-  });
-  
+router.get('/:houseId', (req, res) => {
+	const { houseId } = req.params;
+	db
+		.findByPropertyId(houseId)
+		.then((properties) => {
+			if (properties) {
+				res.status(200).json(properties);
+			} else {
+				res.status(404).json({ error: 'The specified property does not exist.' });
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({ error: `${err}` });
+		});
+});
+
 
 // create property
-router.post('/properties/add', (req, res) => {
-    const { propertyName, propertyAddress, propertyCity, propertyState, propertyZipcode, maxOccupants, bedrooms, bathrooms, square_footage, yearBuilt, uid } = req.body;
-    const property = {
-        propertyName: propertyName,
-        propertyAddress: propertyAddress,
-        propertyCity: propertyCity,
-        propertyState: propertyState,
-        propertyZipcode: propertyZipcode,
-        maxOccupants: maxOccupants,
-        bedrooms: bedrooms,
-        bathrooms: bathrooms,
-        sqFt: sqFt,
-        yearBuilt: yearBuilt,
-    };
-  db.insert(property)
-  .into('properties')
-  .then(houseId => {
-    res.status(201).json(houseId);
-  })
-  .catch(err => {
-    res.status(500).json({ error: `${ err }`});
-  });
+router.post('/', (req, res, next) => {
+	const newProperty = req.body;
+	db
+		.createProperty(newProperty)
+		.then((houseIds) => {
+			db
+				.findByPropertyId(houseIds[0])
+				.then((newProperty) => {
+					res.status(201).json({ newProperty: newProperty.houseId });
+				})
+				.catch((err) => {
+					res.status(500).json({ error: `${err}` });
+				});
+		})
+		.catch((err) => {
+			next('h500', err);
+		});
 });
 
 // edit property
-router.put('/properties/:houseId', (req, res) => {
-    const { houseId } = req.params;
-    
-    db('properties')
-    .where('houseId', houseId)
-    .update(property)
-    .then(property => {
-      res.status(200).json(property);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({ error: `${ err }`});
-    });
+router.put('/:houseId', (req, res, next) => {
+	const { houseId } = req.params;
+	const edit = req.body;
+
+	db
+		.editProperty(houseId, edit)
+		.then((updated) => {
+			if (updated) {
+				res.status(200).json({
+					message: 'Property updated.'
+				});
+			} else {
+				res.status(404).json({ error: 'No property found.' });
+			}
+		})
+		.catch((err) => {
+			next('h500', err);
+		});
 });
 
-// delete property
-router.delete('/properties/:houseId', (req, res) => {
-    const { houseId } = req.params;
-    db('properties')
-      .where('houseId', houseId)
-      .del()
-      .then(property => {
-        if (property === 0) {
-          res.status(505).json({ message: 'Sorry, this property does not exist.' });
-          return;
-        }
-        res.status(200).json(property);
-      })
-      .catch(err => {
-        res.status(500).json({ error: `${err}`});
-      });
-  });
 
+
+// delete property
+router.delete('/:houseId', (req, res) => {
+	const { houseId } = req.params;
+	db
+		.deleteProperty(houseId)
+		.then((property) => {
+			if (property) {
+				res.status(202).json({ message: 'Property deleted.' });
+			} else {
+				res.status(404).json({ error: 'The property specified does not exist.' });
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({ error: `${err}` });
+		});
+});
+
+module.exports = router;
