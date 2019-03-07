8ae44e785f9190a9a84e4da293fb5802fec4d67c
Matthew Meitl <darksamus6@gmail.com>
2019-03-06 18:52:40 -0600

    added props to workordercard

--- front-end/src/components/workorderCard.js
+++ front-end/src/components/workorderCard.js
@@ -3,15 +3,22 @@ import React, { Component } from "react";
 
 
 const workorderCard = (props) => {
+
+
+
     return (
         <div className="property-card">
         <div className="buttons-container">
           <button>Edit</button>
           <button>Delete</button>
         </div>
-        <h1>Address</h1>
-        <h1>Name</h1>
-        <h1>Date</h1>
+        <h1>{this.props.work.property}</h1>
+        <h1>{this.props.work.tenant}</h1>
+        <h1>{this.props.work.description}</h1>
+        <h1>{this.props.work.phone}</h1>
+        <h1>{this.props.work.unsupervisedEntry}</h1>
+        <h1>{this.props.work.status}</h1>
+
       </div>
     )
 }
