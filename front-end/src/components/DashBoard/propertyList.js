9a51697e31713eb5e28b51cc4ac6322b4671c00b
travis-russell <russell.travis@yahoo.com>
2019-03-12 12:05:40 -0500

    changed propertyList

--- front-end/src/components/DashBoard/propertyList.js
+++ front-end/src/components/DashBoard/propertyList.js
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-import PropertyCard from "./propertyCard";
+export default class propertyList extends Component {
+	state = {
+		properties: []
+	};
 
-const PropertyList = () => {
-  return (
-    <div className="right-side">
-      <h2>Properties</h2>
-      <div className="properties-wrapper">
-        <PropertyCard />
-        <PropertyCard />
-        <PropertyCard />
-        <PropertyCard />
-        <PropertyCard />
-        <PropertyCard />
-        <PropertyCard />
-        <PropertyCard />
-        <PropertyCard />
-        <PropertyCard />
-        <PropertyCard />
-      </div>
-    </div>
-  );
-};
-export default PropertyList;
+	componentDidMount() {
+		axios
+			.get('https://tenantly-back.herokuapp.com/properties')
+			.then((response) => this.setState({ properties: response.data }))
+			.catch((err) => {
+				console.error('Server Error', error);
+			});
+	}
+
+	render() {
+		return (
+			<div className="propertiesList">
+				<p className="your-properties">Properties:</p>
+				{this.state.properties.map((property) => {
+					return (
+						<div className="properties" key={property.id}>
+							<Link to={`/properties/${property.id}`}>
+								<h4>{property.propertyName}</h4>
+								<hr />
+								<p>{property.propertyAddress}</p>
+							</Link>
+						</div>
+					);
+				})}
+			</div>
+		);
+	}
+}
