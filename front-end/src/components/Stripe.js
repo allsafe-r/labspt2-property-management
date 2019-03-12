6e1dc501fff44cac0956abea8961c86518e85d6b
Lee Len Formento <leebatef@gmail.com>
2019-03-12 19:08:07 -0400

    axios and onToken functions added. Connected publishable key.

--- front-end/src/components/Stripe.js
+++ front-end/src/components/Stripe.js
@@ -1,5 +1,28 @@
 import React, { Fragment } from "react";
+import StripeCheckout from "react-stripe-checkout";
 import axios from "axios";
 
-
+const Stripe = () => {
+  const publishableKey = "pk_test_uGZWgKZiorkYlZ8MsxYEIrA2";
+   
+  const onToken = token => {
+    const body = {
+      amount: 999,
+      token: token
+  };
+  axios
+      .post("http://localhost:9000/payment", body)
+      .then(response => {
+        console.log(response);
+        alert("Payment Success");
+      })
+      .catch(error => {
+        console.log("Payment Error: ", error);
+        alert("Payment Error");
+      });
+  };
+  return (
+<h1>Test </h1> 
+  );
+};
 export default Stripe;
