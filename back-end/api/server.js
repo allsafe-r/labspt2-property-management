2f2a2e8c8a91a56fdcf2b615064d0f2d9e41d29f
Lee Len Formento <leebatef@gmail.com>
2019-03-12 23:41:39 -0400

    push

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -6,13 +6,17 @@ const properties = require('./properties.js');
 const workOrders = require('./workorders.js');
 const server = express();
 
+
 server.use(express.json(), cors(), helmet());
 server.use('/users', users);
 server.use('/workorders', workOrders);
 server.use('/properties', properties);
 
+
 server.get('/', (req, res) => {
 	res.status(200).send('Hi!');
 });
+
+
 
 module.exports = server;
