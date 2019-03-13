9e48735aa4cd82944e84e8d66239c63aebf95412
travis-russell <russell.travis@yahoo.com>
2019-03-13 11:13:29 -0500

    added secret keys for oauth

--- back-end/api/config/passport-setup.js
+++ back-end/api/config/passport-setup.js
@@ -1,12 +1,12 @@
 const passport = require('passport');
 const GoogleStrat = require('passport-google-oauth20');
+const keys = require('./keys');
 
 passport.use(
-	new GoogleStrat(
-		{
-			//options for strat
-		}
-	),
+	new GoogleStrat({
+		clientID: keys.google.clientID,
+		clientSecret: keys.google.clientSecret
+	}),
 	() => {
 		//passport callback function
 	}
