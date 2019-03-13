e8cdaf82842588dee2926f15d7290dcb67710095
Lee Len Formento <leebatef@gmail.com>
2019-03-13 16:02:41 -0400

    boilerplate

--- front-end/src/components/Stripe.js
+++ front-end/src/components/Stripe.js
@@ -1,39 +1,28 @@
-// import React from "react";
-// import StripeCheckout from "react-stripe-checkout";
-// import axios from "axios";
 
-// const Stripe = () => {
-//     const publishableKey = "pk_test_uGZWgKZiorkYlZ8MsxYEIrA2";
-     
-//     const onToken = token => {
-//       const body = {
-//         amount: 999,
-//         token: token
-//     };
-//     axios
-//         .post("http://localhost:9000/payment", body)
-//         .then(response => {
-//           console.log(response);
-//           alert("Payment Success");
-//         })
-//         .catch(error => {
-//           console.log("Payment Error: ", error);
-//           alert("Payment Error");
-//         });
-//     };
-//     return (
-//       <StripeCheckout
-//       label="Pay with 💳"
-//       name="Tenantly, LLC"
-//       description="Upgrade to a premium account today."
-//       panelLabel="Go Premium" 
-//       amount={999} 
-//       token={onToken}
-//       stripeKey={publishableKey}
-//       image="https://i.ibb.co/L1sx35T/sd.jpg"
-//       billingAddress={false}
-//     />
-//         );
-//       };
+import React, { Component } from 'react';
 
-// export default Stripe;
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+
+  }
+
+  componentDidMount() { 
+
+
+  }
+
+
+
+  render() {
+    return (
+      <div>
+       
+
+      </div>
+    );
+  }
+}
+
+export default App;
