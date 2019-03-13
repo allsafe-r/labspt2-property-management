96db7bca0bbf7447235dabe1b994ce2bd62a3bdb
Lee Len Formento <leebatef@gmail.com>
2019-03-13 00:08:36 -0400

    Axios post

--- front-end/src/components/Stripe.js
+++ front-end/src/components/Stripe.js
@@ -2,4 +2,29 @@ import React, { Fragment } from "react";
 import StripeCheckout from "react-stripe-checkout";
 import axios from "axios";
 
+const Stripe = () => {
+    const publishableKey = "pk_test_uGZWgKZiorkYlZ8MsxYEIrA2";
+     
+    const onToken = token => {
+      const body = {
+        amount: 999,
+        token: token
+    };
+    axios
+        .post("http://localhost:9000/payment", body)
+        .then(response => {
+          console.log(response);
+          alert("Payment Success");
+        })
+        .catch(error => {
+          console.log("Payment Error: ", error);
+          alert("Payment Error");
+        });
+    };
+    return (
+    
+            <h1> Hello </h1>
+        );
+      };
+
 export default Stripe;
