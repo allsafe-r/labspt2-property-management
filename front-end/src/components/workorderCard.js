98552c21fb9aa68501d7dab8866f169ed8c05fad
darksamus6 <darksamus6@gmail.com>
2019-03-13 17:11:15 +0000

    cleaned up workordercard code

--- front-end/src/components/workorderCard.js
+++ front-end/src/components/workorderCard.js
@@ -19,69 +19,59 @@ export default class Workordercard extends Component {
     }
     
 
+//Radio Button Status
 
 inputs = ["Pending" , "In Progress", "Completed"]
 
+//Updates the status of the radio button and sends a put request to the database to reflect the change in work order status
 statushandler = (e) => {
     
-
-
- this.setState({
-    status: e.target.value
-});
-         
-let  updatedworkorder = {
+     this.setState({
+        status: e.target.value
+    });
+             
+    let  updatedworkorder = {
+        
+              property: this.state.property,
+              tenant: this.state.tenant,
+              description: this.state.description,
+              phone: this.state.phone,
+              unsupervisedEntry: this.state.unsupervisedEntry,
+              status: e.target.value
+    }       
     
-          property: this.state.property,
-          tenant: this.state.tenant,
-          description: this.state.description,
-          phone: this.state.phone,
-          unsupervisedEntry: this.state.unsupervisedEntry,
-          status: e.target.value
-}       
-
-
-
-    axios
-      .put(`https://tenantly-back.herokuapp.com/workorders/${this.state.id}`, updatedworkorder)
-      .then(response => {
-      console.log('success')
-        })
-      
-      .catch(error => {
-        console.log(error);
-      });
-      
+        axios
+          .put(`https://tenantly-back.herokuapp.com/workorders/${this.state.id}`, updatedworkorder)
+          .then(response => {
+          console.log('success')
+            })
+          
+          .catch(error => {
+            console.log(error);
+          });
+          
       
 }
 
  render() {
     return (
         <div className="property-card">
-        <div className="buttons-container">
-          <button>Edit</button>
-          <button>Delete</button>
-        </div>
-        <h1>{this.props.work.property}</h1>
-        <h1>{this.props.work.tenant}</h1>
-        <h1>{this.props.work.description}</h1>
-        <h1>{this.props.work.phone}</h1>
-        <h1>{this.props.work.unsupervisedEntry}</h1>
-        
-           { this.inputs.map((values, i) => (
-                <div key={i}>
-                <h1>{values}</h1><input type="radio" name={values} onChange={this.statushandler} value={values} checked={this.state.status === values} />
-                </div>
-            ))}
-           
-          
+            <h1>{this.props.work.property}</h1>
+            <h1>{this.props.work.tenant}</h1>
+            <h1>{this.props.work.description}</h1>
+            <h1>{this.props.work.phone}</h1>
+            <h1>{this.props.work.unsupervisedEntry}</h1>
+            {/* Radio button form */}
+               { this.inputs.map((values, i) => (
+                    <div key={i}>
+                    <h1>{values}</h1><input type="radio" name={values} onChange={this.statushandler} value={values} checked={this.state.status === values} />
+                    </div>
+                ))}
+               
+              
         </div>
 
-        
-
-      
     )
 }
 }
 
-//export default WorkorderCard;
