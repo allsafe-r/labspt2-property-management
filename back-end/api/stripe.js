09fe6fd966895bf45023b4d86f7cc8d436e59881
Lee Len Formento <leebatef@gmail.com>
2019-03-14 13:43:34 -0400

    structure for stripe backend

---
+++ back-end/api/stripe.js
@@ -0,0 +1,15 @@
+// const stripe = require('stripe')('sk_test_H5m1BImFjTdc7oLig2dKoq5A');
+
+// const customer = await stripe.customers.create({
+//   email: 'customer@example.com'
+// });
+
+const express = require('express');
+const db = require('../data/dbConfig');
+const router = express.Router();
+
+router.use(express.json());
+
+
+module.exports = router;
+  
