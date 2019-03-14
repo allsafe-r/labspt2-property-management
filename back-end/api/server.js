0a1252f237a77b234730ea87fb114788a2bbccdf
Lee Len Formento <leebatef@gmail.com>
2019-03-14 13:48:01 -0400

    Added stripe endpoint to server

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -4,12 +4,15 @@ const cors = require('cors');
 const users = require('./users.js');
 const properties = require('./properties.js');
 const workOrders = require('./workorders.js');
+const stripe = require('./stripe.js');
+
 const server = express();
 
 server.use(express.json(), cors(), helmet());
 server.use('/users', users);
 server.use('/workorders', workOrders);
 server.use('/properties', properties);
+server.use('/stripe', stripe);
 
 server.get('/', (req, res) => {
 	res.status(200).send('Hi!');
