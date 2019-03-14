dd68e8f1fad851f7d9097bc8e9882d3cba02957d
travis-russell <russell.travis@yahoo.com>
2019-03-13 22:17:47 -0500

    added bcrypt dependencies and started server.js

---
+++ back-end/api/login.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const db = require('../data/helper/userModal');
+const { generateToken } = require('../auth/bcrypt');
+
+router.post('/register', (req, res, next) => {
+	const creds = req.body;
+	const hash = bcrypt.hashSync(creds.password, 14);
+	creds.password = hash;
+	db
+		.createUser(creds)
+		.then((ids) => {
+			console.log(ids);
+			db
+				.findUserId(ids[0])
+				.then((user) => {
+					res.status(201).json({ username: user.id });
+				})
+				.catch((err) => {
+					res.status(500).json({ err });
+				});
+		})
+		.catch((err) => {
+			next('h500', err);
+		});
+});
+
+router.post('/login', (req, res, next) => {
+	const creds = req.body;
+	db
+		.findUserName(creds.username)
+		.then((user) => {
+			if (user && bcrypt.compareSync(creds.password, user.password)) {
+				const token = generateToken(user);
+				res.json({ Welcome: user.username, userId: user.id, token });
+			} else {
+				res.status(401).json({ message: 'Not Authorized' });
+			}
+		})
+		.catch((err) => {
+			next('h500', err);
+		});
+});
+
+module.exports = router;
--- back-end/api/server.js
+++ back-end/api/server.js
@@ -4,12 +4,14 @@ const cors = require('cors');
 const users = require('./users.js');
 const properties = require('./properties.js');
 const workOrders = require('./workorders.js');
+const login = require('./login.js');
 const server = express();
 
 server.use(express.json(), cors(), helmet());
 server.use('/users', users);
 server.use('/workorders', workOrders);
 server.use('/properties', properties);
+server.use('/api', login);
 
 server.get('/', (req, res) => {
 	res.status(200).send('Hi!');
