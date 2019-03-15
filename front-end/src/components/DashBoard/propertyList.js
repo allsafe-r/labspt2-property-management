6998d59bebd50760446815e83431fca9cfb3b3f1
Victor G <victorgordian103@gmail.com>
2019-03-14 23:36:23 -0700

    Read is working

--- front-end/src/components/DashBoard/propertyList.js
+++ front-end/src/components/DashBoard/propertyList.js
@@ -1,37 +1,37 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default class propertyList extends Component {
-	state = {
-		properties: []
-	};
+  state = {
+    properties: []
+  };
 
-	componentDidMount() {
-		axios
-			.get('https://tenantly-back.herokuapp.com/properties')
-			.then((response) => this.setState({ properties: response.data }))
-			.catch((err) => {
-				console.error('Server Error', error);
-			});
-	}
+  componentDidMount() {
+    axios
+      .get("https://tenantly-back.herokuapp.com/properties")
+      .then(response => this.setState({ properties: response.data }))
+      .catch(err => {
+        console.error("Server Error", err);
+      });
+  }
 
-	render() {
-		return (
-			<div className="propertiesList">
-				<p className="your-properties">Properties:</p>
-				{this.state.properties.map((property) => {
-					return (
-						<div className="properties" key={property.id}>
-							<Link to={`/properties/${property.id}`}>
-								<h4>{property.propertyName}</h4>
-								<hr />
-								<p>{property.propertyAddress}</p>
-							</Link>
-						</div>
-					);
-				})}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="propertiesList">
+        <p className="your-properties">Properties:</p>
+        {this.state.properties.map(property => {
+          return (
+            <div className="properties" key={property.id}>
+              <Link to={`/properties/${property.id}`}>
+                <h4>{property.propertyName}</h4>
+                <hr />
+                <p>{property.propertyAddress}</p>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
