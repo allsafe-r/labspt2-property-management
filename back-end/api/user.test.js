8a297e1b262a9d2295cd9c8280cd5ae14b0c9fba
unknown <kyleteeter12@gmail.com>
2019-03-14 21:34:12 -0500

    Add post and delete tests

--- back-end/api/user.test.js
+++ back-end/api/user.test.js
@@ -34,4 +34,48 @@ describe('Users Route Tests', () => {
         })
     });
    
+    //  Testing Get Request for Users that are Tenants
+    describe('Get Users that are Tenants', () => {
+        it('responds with 200', function(done) {
+            request(app)
+            .get('/users/tenants')
+            .expect(200)
+            .end(done);
+        })
+    });
+
+    // Testing Post function for Users
+    describe('Post property', function () {
+        it('responds with 201', function(done) {
+            request(app)
+            .post('/users')
+            .send({
+                "id": 444,
+                "name": "Paco",
+                "isAdmin": 0,
+                "email": "paco@tenantly.com",
+                "phone": "203-555-1234",
+                "displayName": "Kyle",
+                "emailSubscribe": 0,
+                "textSubscribe": 0,
+                "residence_id": 1,
+                "application": ""
+            })
+            .expect(201)
+            .end(done);
+        })
+    });
+
+    // Testing Delete for Users
+    describe('Delete user', function () {
+        it('responds with 202', function(done) {
+            request(app)
+            .delete('/users/444')
+            .expect(202)
+            .expect({
+                message: 'User deleted'
+            })
+            .end(done);
+        })
+    })
 })
