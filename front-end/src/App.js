b6b6d524ad54f9319fbeaadf40e6227816f1de99
Matthew Meitl <darksamus6@gmail.com>
2019-03-20 21:16:02 -0500

    fixed merges

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,11 +4,7 @@ import Menu from './components/LandingPage/Menu';
 // import Body from './components/LandingPage/Body';
 import IndexPage from './components/LandingPage/IndexPage';
 import Stripe from './components/Stripe';
-<<<<<<< HEAD
 import Workorderform from './components/workorderform';
-=======
-import Imageform from './components/imageform';
->>>>>>> 02149efc3a4aabf7fe571690ee9231eaa58ea9de
 // import Register from './components/DashBoard/register';
 
 class App extends Component {
@@ -18,7 +14,6 @@ class App extends Component {
 				<Menu />
 				{/* <Body /> */}
 				<IndexPage />
-				<Imageform />
 				<Stripe />
 				<Workorderform />
 			</div>
--- front-end/src/components/imageform.js
+++ front-end/src/components/imageform.js
@@ -23,14 +23,9 @@ class Imageform extends React.Component {
         };
         axios.post("http://localhost:9000/images",formData,config)
             .then((response) => {
-<<<<<<< HEAD
                 this.setState({url: response.data})
                 this.updateUrl();
                 
-=======
-                this.setState({url: response.url});
-                alert("The file is successfully uploaded");
->>>>>>> 02149efc3a4aabf7fe571690ee9231eaa58ea9de
             }).catch((error) => {
         });
     }
@@ -48,11 +43,7 @@ class Imageform extends React.Component {
         return (
             <form onSubmit={this.onFormSubmit}>
                 <h1>File Upload</h1>
-<<<<<<< HEAD
                 <input type="file" name="image" onChange= {this.onChange} />
-=======
-                <input type="file" name="myImage" onChange={this.onChange} urlvalue={this.state.url} />
->>>>>>> 02149efc3a4aabf7fe571690ee9231eaa58ea9de
                 <button type="submit">Upload</button>
             </form>
         )
