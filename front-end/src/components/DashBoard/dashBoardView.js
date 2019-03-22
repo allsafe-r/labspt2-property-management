4cfa97ae37b3f48ae8fc29f8c9e6b32deb5d16b8
Matthew Meitl <darksamus6@gmail.com>
2019-03-21 19:39:17 -0500

    added components to dashboard

--- front-end/src/components/DashBoard/dashBoardView.js
+++ front-end/src/components/DashBoard/dashBoardView.js
@@ -2,12 +2,16 @@ import React from "react";
 
 import PropertyList from "./propertyList";
 import SideMenu from "./sideMenu";
+import Workorderlist from '../WorkOrders/workorderList';
+import Workorderform from '../WorkOrders/workorderform';
 
 const DashBoard = () => {
   return (
     <div>
       <SideMenu />
       <PropertyList />
+      <Workorderlist />
+      <Workorderform />
     </div>
   );
 };
