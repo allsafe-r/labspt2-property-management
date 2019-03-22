02f8d2e2c05bc20af57778a1e0275b2fa1d3e286
travis-russell <russell.travis@yahoo.com>
2019-03-21 23:29:10 -0500

    added dynamic properties url

--- front-end/src/components/DashBoard/propertyList.js
+++ front-end/src/components/DashBoard/propertyList.js
@@ -1,46 +1,44 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+const url = process.env.properties || 'http://localhost:9000/properties';
 
-import PropertyCard from "./propertyCard";
+import PropertyCard from './propertyCard';
 
-import "../general.css";
+import '../general.css';
 
 export default class propertyList extends Component {
-  state = {
-    properties: []
-  };
+	state = {
+		properties: []
+	};
 
-  componentDidMount() {
-    axios
-      .get(" http://localhost:9000/properties")
-      .then(response => this.setState({ properties: response.data }))
-      .catch(err => {
-        console.error("Server Error", err);
-      });
-  }
+	componentDidMount() {
+		axios.get(url).then((response) => this.setState({ properties: response.data })).catch((err) => {
+			console.error('Server Error', err);
+		});
+	}
 
-  render() {
-    return (
-      <div className="properties-list">
-        <p className="your-properties">Properties:</p>
-        <div className="properties-container">
-          {this.state.properties.map(property => (
-            <PropertyCard
-              key={property.houseId}
-              name={property.propertyName}
-              address={property.propertyAddress}
-              city={property.propertyCity}
-              state={property.propertyState}
-              zipcode={property.propertyZipcode}
-              id={property.houseId}
-            />
-          ))}
-        </div>
-        <Link to="/add-property">
-          <button>+ Add New Property</button>
-        </Link>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="properties-list">
+				<p className="your-properties">Properties:</p>
+				<div className="properties-container">
+					{this.state.properties.map((property) => (
+						<PropertyCard
+							key={property.houseId}
+							name={property.propertyName}
+							address={property.propertyAddress}
+							city={property.propertyCity}
+							state={property.propertyState}
+							zipcode={property.propertyZipcode}
+							id={property.houseId}
+						/>
+					))}
+				</div>
+				<Link to="/add-property">
+					<button>+ Add New Property</button>
+				</Link>
+			</div>
+		);
+	}
 }
