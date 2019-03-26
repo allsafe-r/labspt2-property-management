21aa4ad0c7384bf4712950112f16ac8510e064f6
travis-russell <russell.travis@yahoo.com>
2019-03-25 20:30:00 -0500

    changed last dynamic url

--- front-end/src/components/WorkOrders/workorderCard.js
+++ front-end/src/components/WorkOrders/workorderCard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-const url = process.env.workOrderCard || `http://localhost:9000/workorders/${this.state.id}`;
+// const url = process.env.workOrderCard || `http://localhost:9000/workorders/${this.state.id}`;
 
 export default class Workordercard extends Component {
 	constructor(props) {
@@ -36,7 +36,7 @@ export default class Workordercard extends Component {
 		};
 
 		axios
-			.put(url, updatedworkorder)
+			.put(`http://localhost:9000/workorders/${this.state.id}`, updatedworkorder)
 			.then((response) => {
 				console.log('success');
 			})
