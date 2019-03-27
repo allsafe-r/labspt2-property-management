4461b9eb1856922a1312b4a179da3b9cf95da4cc
travis-russell <russell.travis@yahoo.com>
2019-03-26 22:08:11 -0500

    workorderCard HC url

--- front-end/src/components/WorkOrders/workorderCard.js
+++ front-end/src/components/WorkOrders/workorderCard.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 // const url = process.env.workOrderCard || `http://localhost:9000/workorders/${this.state.id}`;
-
+const url = `https://tenantly-back.herokuapp.com/${this.state.id}`;
 export default class Workordercard extends Component {
 	constructor(props) {
 		super(props);
@@ -36,7 +36,7 @@ export default class Workordercard extends Component {
 		};
 
 		axios
-			.put(`http://localhost:9000/workorders/${this.state.id}`, updatedworkorder)
+			.put(url, updatedworkorder)
 			.then((response) => {
 				console.log('success');
 			})
