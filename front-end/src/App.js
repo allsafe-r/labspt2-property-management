7574f2bb442eed01fad994b7b0fb2518af0740d2
travis-russell <russell.travis@yahoo.com>
2019-03-28 11:00:25 -0500

    created Links to reg/login on menu page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Menu from './components/LandingPage/Menu';
-// import Body from './components/LandingPage/Body';
 import IndexPage from './components/LandingPage/IndexPage';
 import Stripe from './components/Stripe';
 import DashBoard from './components/DashBoard/dashBoardView';
@@ -11,9 +10,7 @@ import Register from './components/DashBoard/register';
 // const url = process.env.home || 'http://localhost:9000';
 
 const url = 'https://tenantly-back.herokuapp.com';
-
 const axios = require('axios');
-// import Register from './components/DashBoard/register';
 
 class App extends Component {
 	state = {
@@ -26,7 +23,6 @@ class App extends Component {
 
 	authenticate = () => {
 		const token = localStorage.getItem('jwtToken');
-		// console.log(token);
 		const auth = {
 			headers: {
 				Authorization: token
