6009ca6dce6c06d29c886fd146832862bad356b2
Lee Len Formento <leebatef@gmail.com>
2019-03-29 16:36:04 -0400

    few changes

--- front-end/src/components/LandingPage/Menu.js
+++ front-end/src/components/LandingPage/Menu.js
@@ -31,13 +31,13 @@ class Menu extends React.Component {
 			<div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
 				<div className="HeaderGroup">
 					<img src={require('../../assets/images/logo8.svg')} width="180" alt="Logo" />
-					<p>Features</p>
-					<p>Pricing</p>
+					<button className="features">Features</button>
+					<button className="features">Pricing</button>
 					<Link to={'/login'}>
-						<button>Login</button>
+						<button className="login-button-menu">Login</button>
 					</Link>
 					<Link to={'/register'}>
-						<button>Register</button>
+						<button className="register-button-menu">Register</button>
 					</Link>
 				</div>
 			</div>
