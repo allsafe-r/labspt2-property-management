e38f7af59285762aa8ff4b83ea2dda7446f45fe0
unknown <kyleteeter12@gmail.com>
2019-03-30 11:43:32 -0500

    Properties importing into a dropdown list

--- front-end/src/components/adminViews/adminBilling.js
+++ front-end/src/components/adminViews/adminBilling.js
@@ -4,15 +4,25 @@ import Image from '../../assets/images/blue-on-dark.png';
 import { Link } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import PropertyCard from '../properties/addProperty';
-
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import InputLabel from '@material-ui/core/InputLabel';
 
 // const url = process.env.properties || 'http://localhost:9000/properties';
 const url = `https://tenantly-back.herokuapp.com/properties`;
+
+
 
 export default class Billing extends Component {
 	state = {
          properties: []
 	};
+
+	handleInputChange = prop => event => {
+		this.setState({ [prop]: event.target.value });
+	  };
+
 
 	componentDidMount() {
 		axios.get(url).then((response) => this.setState({ properties: response.data })).catch((err) => {
@@ -23,13 +33,31 @@ export default class Billing extends Component {
 	render() {
 		return (
 			<div className="Billing">
-			{/* <Card>
-	 			<div className="propertyBody">
-					<h1>Name: {this.state.property.propertyName}</h1>
-					<h1>Address:{this.state.property.propertyAddress}</h1>
-				</div>
-			</Card> */}
-			<Card>
+				<Card>
+                    <FormControl>
+                      <InputLabel htmlFor="property-native-required">
+                        Select a property to view payment history
+                      </InputLabel>
+                      <Select
+                        native
+                        value={this.state.house_id}
+                        onChange={this.handleInputChange('house_id')}
+                        name="Property"
+                        inputProps={{
+                          id: 'property-native-required',
+                        }}
+                      >
+                        <option value={0} />
+                        {this.state.properties.map((property, index) => (
+                          <option key={index} value={property.propertyName}>
+                            {property.propertyName}
+                          </option>
+                        ))}
+                      </Select>
+                      <FormHelperText>Required</FormHelperText>
+                    </FormControl>
+                  </Card>			
+				  <Card>
 				<a href="https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_Eh0R1RXhYNXEq9z56aVKr04CVDrJvxMc&scope=read_write">
 					<img src={Image} />
 				</a>
