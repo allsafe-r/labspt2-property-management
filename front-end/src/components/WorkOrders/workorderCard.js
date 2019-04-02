d47afca2c69873d997c820202c5d9eece14d3131
Lee Len Formento <leebatef@gmail.com>
2019-04-01 23:55:29 -0400

    added alt prop for img

--- front-end/src/components/WorkOrders/workorderCard.js
+++ front-end/src/components/WorkOrders/workorderCard.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
+// import CardContent from '@material-ui/core/CardContent';
 import CardActionArea from '@material-ui/core/CardActionArea';
 // import { withStyles } from '@material-ui/core/styles';
 import Radio from '@material-ui/core/Radio';
