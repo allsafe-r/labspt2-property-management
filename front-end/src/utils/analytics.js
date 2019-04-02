46c712daf4cbb07d396b76d58edf24863cb4fcba
Lee Len Formento <leebatef@gmail.com>
2019-04-02 16:00:20 -0400

    build

--- front-end/src/utils/analytics.js
+++ front-end/src/utils/analytics.js
@@ -5,6 +5,6 @@ export const initGA = () => {
     ReactGA.initialize('UA-137488840-1')
 }
 export const logPageView = () => {
-     ReactGA.set({ page: window.location.pathname })
+    ReactGA.set({ page: window.location.pathname })
     ReactGA.pageview(window.location.pathname)
 }
