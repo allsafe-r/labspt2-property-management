13a398b9319493219c23e6ec7366afe3cf9fd7af
travis-russell <russell.travis@yahoo.com>
2019-04-04 08:18:08 -0500

    fixed typo with dropColumn

--- back-end/data/migrations/05_addAlertsToProps.js
+++ back-end/data/migrations/05_addAlertsToProps.js
@@ -6,6 +6,6 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
 	return knex.schema.table('properties', function(table) {
-		table.specificType('alerts', []);
+		table.dropColumn('alerts');
 	});
 };
