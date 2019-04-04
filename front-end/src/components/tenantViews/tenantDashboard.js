37b05a52c3c6dbee5d1dd8eafc4ca2daccc6d804
travis-russell <russell.travis@yahoo.com>
2019-04-04 10:31:30 -0500

    properly setting houseinfo, contactinfo, and alerts

--- front-end/src/components/tenantViews/tenantDashboard.js
+++ front-end/src/components/tenantViews/tenantDashboard.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 export default class tenantDashboard extends Component {
 	state = {
 		houseId: 1,
+		residenceOwner: 1,
 		alerts: [],
 		address: '',
 		contact: '',
@@ -15,28 +16,41 @@ export default class tenantDashboard extends Component {
 
 	componentDidMount() {
 		let id = localStorage.getItem('userId');
+
+		// go into users to find which residence you live at
 		axios
 			.get(`https://tenantly-back.herokuapp.com/users/${id}`)
 			.then((user) => {
 				// console.log(user);
-				this.setState({ houseId: user.residenceId });
+				this.setState({ houseId: user.data.residenceId });
 			})
-			.catch((err) => console.log(err));
-
-		axios.get(`https://tenantly-back.herokuapp.com/properties//${this.state.houseId}`).then((house) => {
-			this.setState({ address: house.address });
-		});
-		// console.log(this.state);
-		axios.get(`http://localhost:9000/alerts`).then((res) => {
-			let alertsObj = res.data.filter((alert) => alert.houseId == this.state.houseId);
-			let alertsArr = [];
-			alertsObj.forEach((alert) => {
-				alertsArr.push(alert.alert);
-			});
-			this.setState({ alerts: alertsArr });
-		});
-
-		console.log(this.state);
+			// go into users residence, grab some information and set it to state, grab owner of residence to supply rest of information
+			.then(
+				axios
+					.get(`https://tenantly-back.herokuapp.com/properties/${this.state.houseId}`)
+					.then((res) => {
+						let property = res.data;
+						this.setState({ residenceOwner: property.owner, address: property.propertyAddress });
+					})
+					// find the owner of logged in users residence to supply contact info for owner
+					.then(
+						axios.get(`https://tenantly-back.herokuapp.com/users/${this.state.residenceOwner}`).then((res) => {
+							let owner = res.data;
+							this.setState({ contact: owner.phone, contactEmail: owner.email });
+						})
+					)
+			)
+			.then(
+				// go into alerts and grab each alerts where the houseId matches logged in users residence, map over them to create a text array
+				axios.get(`http://localhost:9000/alerts`).then((res) => {
+					let alertsObj = res.data.filter((alert) => alert.houseId == this.state.houseId);
+					let alertsArr = [];
+					alertsObj.forEach((alert) => {
+						alertsArr.push(alert.alert);
+					});
+					this.setState({ alerts: alertsArr });
+				})
+			);
 	}
 	render() {
 		return (
@@ -50,15 +64,15 @@ export default class tenantDashboard extends Component {
 					</div>
 					<div>
 						{this.state.alerts.map((alert) => {
-							return <li>{alert}</li>;
+							return <li key={alert.id}>{alert}</li>;
 						})}
 					</div>
 				</div>
 				<div className="right-tenant-dash">
-					<div>Address{this.state.address}</div>
-					<div>Contact Info{this.state.contact}</div>
-					<div>Contact Email{this.state.contactEmail}</div>
-					<div>24/7 Phone{this.state.maintenancePhone}</div>
+					<div>Address: {this.state.address}</div>
+					<div>Contact Info: {this.state.contact}</div>
+					<div>Contact Email: {this.state.contactEmail}</div>
+					<div>24/7 Phone: {this.state.maintenancePhone}</div>
 				</div>
 			</div>
 		);
