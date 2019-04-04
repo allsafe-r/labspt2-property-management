6192dc0ef9b1df1cfaeee25a5455aed917e04ea7
travis-russell <russell.travis@yahoo.com>
2019-04-04 09:19:12 -0500

    alertsModal

---
+++ back-end/data/helper/alertsModal.js
@@ -0,0 +1,39 @@
+const db = require('../../knex');
+
+module.exports = {
+	getAlerts,
+	findByAlertId,
+	getAlert,
+	createAlert,
+	deleteAlert,
+	editAlert,
+	findByHouseId
+};
+
+function getAlerts() {
+	return db('alerts');
+}
+
+function findByAlertId(id) {
+	return db('alerts').where({ id }).first();
+}
+
+function findByHouseId(houseId) {
+	return db('properties').where({ houseId }).first();
+}
+
+function getAlert(id) {
+	return db('alerts').where({ id }).first();
+}
+
+function createAlert(alert) {
+	return db('alerts').insert(alert, 'id');
+}
+
+function deleteAlert(id) {
+	return db('alerts').where({ id }).del();
+}
+
+function editAlert(id, alert) {
+	return db('alerts').where({ id }).update(alert, 'id');
+}
