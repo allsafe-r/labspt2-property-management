f41508fd6a5c2f67a8781ca779714dd9c9acbef1
travis-russell <russell.travis@yahoo.com>
2019-04-04 09:21:05 -0500

    get alerts endpoint

--- back-end/api/alerts.js
+++ back-end/api/alerts.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../data/helper/alertsModal');
+
+// Get all alerts
+router.get('/', (req, res) => {
+	db.getAlerts().then((alerts) => res.status(200).json(alerts)).catch((err) => {
+		res.status(500).json({ error: `${err}` });
+	});
+});
