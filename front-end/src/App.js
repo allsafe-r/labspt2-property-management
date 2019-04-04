9e9cbb48ab1308564724bb897ac66d4f58a8c310
travis-russell <russell.travis@yahoo.com>
2019-04-04 12:05:19 -0500

    deleted duplicate componentDidMounts

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,105 +1,95 @@
-import React, { Component } from "react";
-import "./assets/css/App.css";
-import Menu from "./components/LandingPage/Menu";
-import IndexPage from "./components/LandingPage/IndexPage";
-import Stripe from "./components/Stripe";
-import RouteContainer from "./components/routeContainer";
-import Login from "./components/auth/login";
-import Register from "./components/auth/register";
+import React, { Component } from 'react';
+import './assets/css/App.css';
+import Menu from './components/LandingPage/Menu';
+import IndexPage from './components/LandingPage/IndexPage';
+import Stripe from './components/Stripe';
+import RouteContainer from './components/routeContainer';
+import Login from './components/auth/login';
+import Register from './components/auth/register';
 // import RegisterTwo from './components/auth/register2';
-import Pricing from "./components/LandingPage/Pricing";
-import { logPageView } from "./utils/analytics";
-import { initGA } from "./utils/analytics";
-import LandingView from "./components/LandingPage/LandingView";
-import { Route } from "react-router-dom";
-import { Link } from "react-router-dom";
+import Pricing from './components/LandingPage/Pricing';
+import { logPageView } from './utils/analytics';
+import { initGA } from './utils/analytics';
+import LandingView from './components/LandingPage/LandingView';
+import { Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 // const url = process.env.home || 'http://localhost:9000';
 
-const url = "https://tenantly-back.herokuapp.com";
-const axios = require("axios");
+const url = 'https://tenantly-back.herokuapp.com';
+const axios = require('axios');
 
 class App extends Component {
-  state = {
-    loggedIn: false
-  };
+	state = {
+		loggedIn: false
+	};
 
-  componentDidMount() {
-    this.authenticate();
-  }
+	componentDidMount() {
+		initGA();
+		logPageView();
+		this.authenticate();
+	}
 
-  componentDidMount() {
-    initGA();
-    logPageView();
-    this.authenticate();
-  }
+	authenticate = () => {
+		const token = localStorage.getItem('jwtToken');
+		const auth = {
+			headers: {
+				Authorization: token
+			}
+		};
 
-  authenticate = () => {
-    const token = localStorage.getItem("jwtToken");
-    const auth = {
-      headers: {
-        Authorization: token
-      }
-    };
+		if (token) {
+			axios
+				.get(url, auth)
+				.then((res) => {
+					if (res.data) {
+						this.setState({ loggedIn: true });
+					} else {
+						throw new Error();
+					}
+				})
+				.catch((err) => this.props.history.push('/login'));
+		} else {
+			console.log('Register and/or login to receive a token');
+		}
+	};
 
-    if (token) {
-      axios
-        .get(url, auth)
-        .then(res => {
-          if (res.data) {
-            this.setState({ loggedIn: true });
-          } else {
-            throw new Error();
-          }
-        })
-        .catch(err => this.props.history.push("/login"));
-    } else {
-      console.log("Register and/or login to receive a token");
-    }
-  };
+	logOut = () => {
+		localStorage.removeItem('jwtToken');
+		localStorage.removeItem('userId');
+		this.setState({ loggedIn: false });
+	};
 
-  logOut = () => {
-    localStorage.removeItem("jwtToken");
-    localStorage.removeItem("userId");
-    this.setState({ loggedIn: false });
-  };
+	render() {
+		if (this.state.loggedIn === false) {
+			return (
+				<div>
+					<Route exact path={'/'} component={LandingView} />
+					<Route exact path={'/register'} component={Register} />
+					<Route path={'/register/plan'} component={Pricing} />
+					<Route exact path={'/login'} render={(props) => <Login {...props} authenticate={this.authenticate} />} />
+				</div>
+			);
+		} else {
+			return (
+				<div>
+					<div className="top-bar">
+						<Link to={'/'}>
+							<button onClick={this.logOut}>Logout</button>
+						</Link>
+						<Link to={'/admin/properties'}>
+							<button>Development Purposes - I'm an admin!</button>
+						</Link>
+						<Link to={'/tenant/dashboard'}>
+							<button>Development Purposes - I'm a tenant!</button>
+						</Link>
+					</div>
+					<RouteContainer />
 
-  render() {
-    if (this.state.loggedIn === false) {
-      return (
-        <div>
-          <Route exact path={"/"} component={LandingView} />
-          <Route exact path={"/register"} component={Register} />
-          <Route path={"/register/plan"} component={Pricing} />
-          <Route
-            exact
-            path={"/login"}
-            render={props => (
-              <Login {...props} authenticate={this.authenticate} />
-            )}
-          />
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <div className="top-bar">
-            <Link to={"/"}>
-              <button onClick={this.logOut}>Logout</button>
-            </Link>
-            <Link to={"/admin/properties"}>
-              <button>Development Purposes - I'm an admin!</button>
-            </Link>
-            <Link to={"/tenant/dashboard"}>
-              <button>Development Purposes - I'm a tenant!</button>
-            </Link>
-          </div>
-          <RouteContainer />
-
-          <Stripe />
-        </div>
-      );
-    }
-  }
+					<Stripe />
+				</div>
+			);
+		}
+	}
 }
 
 export default App;
