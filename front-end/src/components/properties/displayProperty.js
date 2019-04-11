f6430b59f2f30fc1370c1d0105da9f184e99b8a4
Lee Len Formento <leebatef@gmail.com>
2019-04-11 10:01:33 -0500

    importing grid

--- front-end/src/components/properties/displayProperty.js
+++ front-end/src/components/properties/displayProperty.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import DeleteModal from './deleteModal';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormLabel from '@material-ui/core/FormLabel';
+
 // const url = process.env.getProperty || `http://localhost:9000/properties/${id}`;
 //const url = `https://tenantly-back.herokuapp.com/properties/${id}`;
 
