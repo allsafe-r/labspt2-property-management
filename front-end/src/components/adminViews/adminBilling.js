4a8a6c3ff73e43cac470f2c43784405fc989c5ed
travis-russell <russell.travis@yahoo.com>
2019-04-11 08:14:11 -0500

    fixed password on seeds

--- front-end/src/components/adminViews/adminBilling.js
+++ front-end/src/components/adminViews/adminBilling.js
@@ -27,7 +27,7 @@ export default class Billing extends Component {
 		console.log(this.state.house_id);
 		this.setState({value: event.target.value});
 		axios
-		.get(`http://localhost:9000/billing/${this.state.value}`)
+		.get(`https://tenantly-back.herokuapp.com/billing/${this.state.value}`)
 			.then((response) => {
 				this.setState({ propertySelected: response.data });
 			})
@@ -37,7 +37,7 @@ export default class Billing extends Component {
 	  };
 
 	setBilling = () => {
-		axios.get(url2).then((response) => this.setState({ billing: response.data }, function () {
+		axios.get(url).then((response) => this.setState({ billing: response.data }, function () {
 			console.log(this.state.billing);
 		})).catch((err) => {
 			console.error('Server Error', err);
