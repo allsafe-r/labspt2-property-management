0dc34840679ed5581c9c47a415421ca75ba0c838
travis-russell <russell.travis@yahoo.com>
2019-04-10 21:32:45 -0500

    fixed seeds

--- back-end/data/migrations/06_billingHistory.js
+++ back-end/data/migrations/06_billingHistory.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
 	return knex.schema.createTable('billing', (table) => {
 		table.increments();
         table.integer('propertyID').notNullable().references('houseId').inTable('properties');
-        table.integer('propertyName').notNullable().references('propertyName').inTable('properties');
+        table.text('propertyName').notNullable().references('propertyName').inTable('properties');
 		table.integer('date').notNullable();
 		table.string('amount').notNullable();
 	});
