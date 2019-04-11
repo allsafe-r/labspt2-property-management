774d78bb8fa911fd3c416039b077fcffc0131955
Lee Len Formento <leebatef@gmail.com>
2019-04-11 18:17:06 -0500

    imported withstyles, card

--- front-end/src/components/properties/propertyCard.js
+++ front-end/src/components/properties/propertyCard.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Card from '@material-ui/core/Card';
+import { withStyles } from '@material-ui/core/styles';
 import { Link } from "react-router-dom";
 
 const PropertyCard = props => {
   return (
-    <div className="noteInfo">
       <Link to={`/admin/view-property/${props.id}`}>
         <h1>{props.name}</h1>
         <h1>{props.address}</h1>
@@ -11,8 +12,7 @@ const PropertyCard = props => {
         <h1>{props.state}</h1>
         <h1>{props.zipcode}</h1>
       </Link>
-    </div>
   );
 };
 
-export default PropertyCard;
+export default withStyles(styles)(PropertyCard);
