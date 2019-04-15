a74f87edf635d62129a1eb4a46d6bf5852d087f2
Lee Len Formento <leebatef@gmail.com>
2019-04-15 12:06:09 -0400

    imported upload icon

--- front-end/src/components/WorkOrders/imageform.js
+++ front-end/src/components/WorkOrders/imageform.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import Button from '@material-ui/core/Button';
 
 const axios = require('axios');
