77221a60ff00b6419f5f46b7b0efb7b8b69cac79
darksamus6 <darksamus6@gmail.com>
2019-04-15 16:53:34 +0000

    redoing pull requested. Fixed some bugs that I missed before

--- front-end/src/components/tenantViews/tenantPayments.js
+++ front-end/src/components/tenantViews/tenantPayments.js
@@ -6,10 +6,12 @@ import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardHeader from '@material-ui/core/CardHeader';
 import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 import axios from 'axios';
 import './../WorkOrders/workorders.css';
 
-const url = 'http://localhost:9000/stripe/charges'
+const url = 'https://tenantly-back.herokuapp.com/stripe/charges'
 // const url = 'https://tenantly-back.herokuapp.com/stripe/charges';
 
 export default class tenantPayments extends Component {
@@ -42,7 +44,7 @@ export default class tenantPayments extends Component {
 	
 	
 	
-	render() {
+	render(){
 		const fonts = [{ cssSrc: "https://fonts.googleapis.com/css?family=Podkova:400" }]
 		return (
 		  <StripeProvider apiKey="pk_test_uGZWgKZiorkYlZ8MsxYEIrA2">
@@ -52,7 +54,7 @@ export default class tenantPayments extends Component {
 			<Card className = 'form-card'>
 			  <h1>React Stripe Elements Example</h1>
 			  <Elements fonts={fonts}>
-				<CheckoutForm charge={this.updatestate} />
+				<CheckoutForm  charge={this.updatestate} />
 				
 			  </Elements>
 			  </Card>
@@ -60,14 +62,21 @@ export default class tenantPayments extends Component {
 				<Grid item sm={12} lg={6}>
 
 					<Card>
-					 
+					<Paper elevation={1}>
 						{this.state.charges.map((charge) => 
+
+<div>						
+						<CardHeader variant='h1' title={charge.billing_details.name}/>
+						<Divider/>
+						<Typography variant='h4'>Date: {this.convertToTime(charge.created)}</Typography>
+						<Divider/>
+						<Typography variant='h4' component='h2'>Amount Paid:${charge.amount}.00</Typography>
+						<Divider/>
 						
-						<ul><li>Date: {this.convertToTime(charge.created)}</li>
-						<li>Name: {charge.billing_details.name}</li>
-						<li>Amount Paid:${charge.amount}.00</li>
-						{/* <Divider /> */}
-						</ul>
+
+						
+						
+</div>						
 						
 						
 						
@@ -76,7 +85,7 @@ export default class tenantPayments extends Component {
 						
 
 
-
+                       </Paper>
 						</Card>				
 			</Grid>
 
