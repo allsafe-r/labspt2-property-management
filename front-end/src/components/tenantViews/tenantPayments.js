dd136ce8e06518cf5cd4b121b7e8df530e542e96
Lee Len Formento <leebatef@gmail.com>
2019-04-16 00:57:55 -0400

    change

--- front-end/src/components/tenantViews/tenantPayments.js
+++ front-end/src/components/tenantViews/tenantPayments.js
@@ -6,42 +6,39 @@ import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardHeader from '@material-ui/core/CardHeader';
 import Divider from '@material-ui/core/Divider';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
+import axios from 'axios';
 import './../WorkOrders/workorders.css';
 
-import axios from 'axios'
+const url = 'https://tenantly-back.herokuapp.com/stripe/charges';
 
 export default class tenantPayments extends Component {
 	constructor(props) {
     super(props);
     this.state = {
-      charges: [
-				{ 
-					name: 'Matt',
-					amount: 120000
-				},
-				{ 
-					name: 'Matt',
-					amount: 120000
-				},
-				{ 
-					name: 'Matt',
-					amount: 120000
-				},
-			]
+      charges: []
     };
     
 	}
-	
-	addcharge = (charge) => {
-		const currentcharges = this.state.charges;
-		let newcharge = charge;
-		currentcharges.push(newcharge)
-		this.setState({
-			charges: currentcharges,
-		})
+
+	componentDidMount() {
+		axios.get(url).then((response) => this.setState({ charges: response.data })).catch((error) => {
+			console.error('Server Error', error);
+		});
 	}
+	
+
+	updatestate =() => {
+		axios.get(url).then((response) => this.setState({ charges: response.data })).catch((error) => {
+			console.error('Server Error', error);
+		});
+	}
+
+	convertToTime =(e) =>{
+		const d = new Date(e * 1000)
+		return d.toLocaleString();
+}
+
+	
 	
 	
 	render() {
@@ -54,7 +51,7 @@ export default class tenantPayments extends Component {
 			<Card className = 'form-card'>
 			  <h1>React Stripe Elements Example</h1>
 			  <Elements fonts={fonts}>
-				<CheckoutForm  charge={this.addcharge} />
+				<CheckoutForm charge={this.updatestate} />
 				
 			  </Elements>
 			  </Card>
@@ -62,16 +59,14 @@ export default class tenantPayments extends Component {
 				<Grid item sm={12} lg={6}>
 
 					<Card>
-					<Paper elevation={1}>
+					 
 						{this.state.charges.map((charge) => 
-
-<div>						
-						<CardHeader variant='h1' title={charge.name}/>
 						
-
-						<Typography variant='h4' component='h2'>Amount: {charge.amount}</Typography>
-						
-</div>						
+						<ul><li>Date: {this.convertToTime(charge.created)}</li>
+						<li>Name: {charge.billing_details.name}</li>
+						<li>Amount Paid:${charge.amount}.00</li>
+						<Divider />
+						</ul>
 						
 						
 						
@@ -80,7 +75,7 @@ export default class tenantPayments extends Component {
 						
 
 
-                       </Paper>
+
 						</Card>				
 			</Grid>
 
