f186eda348f2764cb39e8105403e00d7ca56aca3
travis-russell <russell.travis@yahoo.com>
2019-04-16 22:17:42 -0500

    get single alert

--- back-end/api/alerts.js
+++ back-end/api/alerts.js
@@ -8,3 +8,20 @@ router.get('/', (req, res) => {
 		res.status(500).json({ error: `${err}` });
 	});
 });
+
+// Get an alert
+router.get('/:id', (req, res) => {
+	const { id } = req.params;
+	db
+		.getAlert(id)
+		.then((alerts) => {
+			if (alerts) {
+				res.status(200).json(alerts);
+			} else {
+				res.status(404).json({ error: 'The specified alert does not exist.' });
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({ error: `${err}` });
+		});
+});
