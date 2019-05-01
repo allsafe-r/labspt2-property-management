7424f65b66c6dba59f733789029876c4fcb60e5d
Lee Len Formento <leebatef@gmail.com>
2019-05-01 16:28:44 -0400

    second column footer

--- front-end/src/components/LandingPage/Footer.js
+++ front-end/src/components/LandingPage/Footer.js
@@ -12,8 +12,13 @@ const Footer = () => (
                             <li><a href="#">Pricing </a></li>
                             <li><a href="#">Careers </a></li>
 						</ul>
-
-
+                        <div className="box">
+						<ul className="f-nav">
+							<li><a href="#">For Tenants </a></li>
+							<li><a href="#">For Landlords</a></li>
+							<li><a href="#">For Partners</a></li>
+						</ul>
+                        </div>
 					</div>
 				</div>
 			</div>
