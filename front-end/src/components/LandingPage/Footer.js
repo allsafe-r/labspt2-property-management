5a52a94f1787c1ba9da386bfd9d6146e012170e9
Lee Len Formento <leebatef@gmail.com>
2019-05-01 16:25:41 -0400

    barebones of footer

--- front-end/src/components/LandingPage/Footer.js
+++ front-end/src/components/LandingPage/Footer.js
@@ -2,10 +2,15 @@ import React from 'react';
 import "../../assets/css/general.css"
 
 const Footer = () => (
-    <div>
+    		<div className="f-area">
+			<div className="page-holder">
+				<div className="f-area-holder">
+					<div className="box">
 
-    </div>
-
+					</div>
+				</div>
+			</div>
+		</div>
 )
 
 export default Footer;
