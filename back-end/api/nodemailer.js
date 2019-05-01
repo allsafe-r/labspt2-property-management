0255b96d01fa8050da54e78ac9035956acb23ea3
travis-russell <russell.travis@yahoo.com>
2019-04-30 20:32:47 -0500

    added config/keys to process.env heroku and took off gitignore

--- back-end/api/nodemailer.js
+++ back-end/api/nodemailer.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const nodemailer = require('nodemailer');
-const creds = require('./config/keys');
+const creds = require('./config/keys.js');
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.ethereal.email',
