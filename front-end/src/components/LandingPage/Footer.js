b7abf511782b0a5c23444fc7ff6e0f6cdd0f6b0b
Lee Len Formento <leebatef@gmail.com>
2019-05-01 16:27:26 -0400

    first column footer

--- front-end/src/components/LandingPage/Footer.js
+++ front-end/src/components/LandingPage/Footer.js
@@ -6,6 +6,13 @@ const Footer = () => (
 			<div className="page-holder">
 				<div className="f-area-holder">
 					<div className="box">
+                    <h2>Tenantly</h2>
+						<ul>
+                            <li><a href="#">About </a></li>
+                            <li><a href="#">Pricing </a></li>
+                            <li><a href="#">Careers </a></li>
+						</ul>
+
 
 					</div>
 				</div>
