89bf7ff04cd3170af232767fda79a11a29bc9ede
Victor G <victorgordian103@gmail.com>
2019-05-01 16:50:57 -0700

    checking

--- front-end/src/components/AddTenant/addTenant.js
+++ front-end/src/components/AddTenant/addTenant.js
@@ -5,18 +5,32 @@ import HousingInfo from "./infoCard";
 
 import "../../assets/css/general.css";
 
-const AddTenant = () => {
-  return (
-    <div className="addTenant-container">
-      <div className="tenantInfo-container">
-        <TenantCard />
-        <TenantCard />
+class AddTenant extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTenant: false
+    };
+  }
+  addAnotherTenant() {
+    this.setState({ newTenant: true });
+  }
+  render() {
+    return (
+      <div className="addTenant-container">
+        <div className="tenantInfo-container">
+          <TenantCard />
+          <TenantCard />
+          <div className="tenantAdd" onclick={this.newTenant}>
+            <h1>Add Tenant</h1>
+          </div>
+        </div>
+        <div className="contract-container">
+          <HousingInfo />
+        </div>
       </div>
-      <div className="contract-container">
-        <HousingInfo />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default AddTenant;
