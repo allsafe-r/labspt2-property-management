15b5c08a0ec0f7e95fba5f7e0a7258b38170c1ff
Lee Len Formento <leebatef@gmail.com>
2019-05-06 14:18:33 -0400

    title style

--- front-end/src/components/LandingPage/Cell.js
+++ front-end/src/components/LandingPage/Cell.js
@@ -17,6 +17,12 @@ const CellImage = styled.div`
     background-size: 60px;
 `
 
+const CellTitle = styled.div`
+    font-size: 24px;
+    border-bottom: 1px solid rgba(0,0,0, 0.1);
+    padding: 30px 0;
+`
+
 const Cell = props => (
     <CellGroup>
 
