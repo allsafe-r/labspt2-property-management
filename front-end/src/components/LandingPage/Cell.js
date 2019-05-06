c3854cc093265537311bdf49b2af8e28e19b3b03
Lee Len Formento <leebatef@gmail.com>
2019-05-06 14:17:53 -0400

    styled component divs

--- front-end/src/components/LandingPage/Cell.js
+++ front-end/src/components/LandingPage/Cell.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const CellGroup = styled.div`
+    display: grid;
+    grid-template-columns: 60px auto;
+    grid-gap: 25px;
+    align-items: center;
+`
+
 const Cell = props => (
     <div>
     </div>
