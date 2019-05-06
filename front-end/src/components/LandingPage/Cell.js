5dc07aab543d24dca20bb287a2089bd26f1bc838
Lee Len Formento <leebatef@gmail.com>
2019-05-06 10:46:20 -0400

    title added and styled

--- front-end/src/components/LandingPage/Cell.js
+++ front-end/src/components/LandingPage/Cell.js
@@ -17,11 +17,16 @@ const CellImage = styled.div`
     background-size: 60px;
 `
 
+const CellTitle = styled.div`
+    font-size: 24px;
+    border-bottom: 1px solid rgba(0,0,0, 0.1);
+    padding: 30px 0;
+`
+
 const Cell = props => (
     <CellGroup>
         <CellImage image={props.image}></CellImage>
-
-
+        <CellTitle>{props.title}</CellTitle>
     </CellGroup>
 
 )
