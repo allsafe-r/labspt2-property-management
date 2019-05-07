fa717c6905cfd7d02f7a49b9cb802cec9ee9f02f
travis-russell <russell.travis@yahoo.com>
2019-05-07 18:18:49 -0500

    removed unnec. arg

--- front-end/src/components/auth/login.js
+++ front-end/src/components/auth/login.js
@@ -1,89 +1,89 @@
-import React, { Component } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import "../../assets/css/general.css";
-import logo from "../../assets/images/logo.png";
-const url = "https://tenantly-back.herokuapp.com/api/login";
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import '../../assets/css/general.css';
+import logo from '../../assets/images/logo.png';
+const url = 'https://tenantly-back.herokuapp.com/api/login';
 // const url = 'http://localhost:9000/api/login';
-const decode = require("jwt-decode");
+const decode = require('jwt-decode');
 
 class Login extends Component {
-  state = {
-    email: "",
-    password: "",
-    search: false
-  };
+	state = {
+		email: '',
+		password: '',
+		search: false
+	};
 
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+	onChange = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
 
-  onSubmit = e => {
-    const acc = { email: this.state.email, password: this.state.password };
-    this.setState({ search: true });
-    e.preventDefault();
-    axios
-      .post(url, acc)
-      .then(res => {
-        localStorage.setItem("jwtToken", res.data.token);
-        decode(localStorage.getItem("jwtToken")).isAdmin
-          ? this.props.history.push("/properties")
-          : this.props.history.push("/dashboard");
-        this.props.authenticate(res.data.isAdmin);
-      })
-      .catch(err => {
-        console.log(err);
-        alert("This e-mail and/or password does not match our records");
-        this.setState({ search: false });
-      });
-  };
+	onSubmit = (e) => {
+		const acc = { email: this.state.email, password: this.state.password };
+		this.setState({ search: true });
+		e.preventDefault();
+		axios
+			.post(url, acc)
+			.then((res) => {
+				localStorage.setItem('jwtToken', res.data.token);
+				decode(localStorage.getItem('jwtToken')).isAdmin
+					? this.props.history.push('/properties')
+					: this.props.history.push('/dashboard');
+				this.props.authenticate();
+			})
+			.catch((err) => {
+				console.log(err);
+				alert('This e-mail and/or password does not match our records');
+				this.setState({ search: false });
+			});
+	};
 
-  render() {
-    return (
-      <div className="form-container">
-        <form onSubmit={this.onSubmit}>
-          <Link to={"/"}>
-            <img className="logo-login" src={logo} alt="Logo" />
-          </Link>
-          <div className="user-container">
-            <input
-              placeholder="email"
-              name="email"
-              value={this.state.email}
-              onChange={this.onChange}
-              type="text"
-              required
-            />
-          </div>
-          <div className="password-container">
-            <input
-              placeholder="password"
-              name="password"
-              value={this.state.password}
-              onChange={this.onChange}
-              type="password"
-              required
-            />
-          </div>
-          <div>
-            {this.state.search ? (
-              <div className="lds-ring">
-                <div />
-              </div>
-            ) : (
-              <button className="form__button ">Login</button>
-            )}
-          </div>
-          <div className="no-account">
-            <p className="login-p">Don't have an account?</p>
-            <Link to={"/register"}>
-              <button className="register-button">Register</button>
-            </Link>
-          </div>
-        </form>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="form-container">
+				<form onSubmit={this.onSubmit}>
+					<Link to={'/'}>
+						<img className="logo-login" src={logo} alt="Logo" />
+					</Link>
+					<div className="user-container">
+						<input
+							placeholder="email"
+							name="email"
+							value={this.state.email}
+							onChange={this.onChange}
+							type="text"
+							required
+						/>
+					</div>
+					<div className="password-container">
+						<input
+							placeholder="password"
+							name="password"
+							value={this.state.password}
+							onChange={this.onChange}
+							type="password"
+							required
+						/>
+					</div>
+					<div>
+						{this.state.search ? (
+							<div className="lds-ring">
+								<div />
+							</div>
+						) : (
+							<button className="form__button ">Login</button>
+						)}
+					</div>
+					<div className="no-account">
+						<p className="login-p">Don't have an account?</p>
+						<Link to={'/register'}>
+							<button className="register-button">Register</button>
+						</Link>
+					</div>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default Login;
