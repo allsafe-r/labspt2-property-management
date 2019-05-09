e6b3be4b13ef82a344b1f7ec091175f693dc15dd
Victor G <victorgordian103@gmail.com>
2019-05-08 20:51:06 -0700

    Tenant Info style is done so it can go with the other cards in the site

--- front-end/src/components/AddTenant/infoCard.js
+++ front-end/src/components/AddTenant/infoCard.js
@@ -1,28 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-const HousingInfo = () => {
-  return (
-    <div className="info-container">
-      <h1>Housing Info</h1>
-      <div className="housing-info ">
-        <div className="start-end">
-          <input type="date" name="start" />
-          <input type="date" name="end" />
-        </div>
-        <div className="option-properties yes">
-          <select>
-            <option value="property-1">property 1</option>
-            <option value="property-2">property 2</option>
-            <option value="property-3">property 3</option>
-            <option value="property-4">property 4</option>
-          </select>
-        </div>
-        <div className="info-button yes">
-          <button>Send Contract</button>
+export default class HousingInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tenant: []
+    };
+  }
+
+  // componentDidMount() {
+  //   const id = this.props.match.params.id;
+  //   this.fetchApp(id);
+  // }
+
+  // fetchApp = id => {
+  //   axios
+  //     .get(`https://tenantly-back.herokuapp.com/users/tenants/${id}`)
+  //     .then(response => {
+  //       this.setState({ tenant: response.data });
+  //     })
+  //     .catch(error => {
+  //       console.error(error);
+  //     });
+  // };
+
+  render() {
+    return (
+      <div className="info-container">
+        <h1>Housing Info</h1>
+        <div className="housing-info ">
+          <div className="start-end">
+            <input type="date" name="start" />
+            <input type="date" name="end" />
+          </div>
+          <div className="option-properties yes">
+            <select>
+              <option value="property-1">property 1</option>
+              <option value="property-2">property 2</option>
+              <option value="property-3">property 3</option>
+              <option value="property-4">property 4</option>
+            </select>
+          </div>
+          <div className="info-button yes">
+            <button>Send Contract</button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default HousingInfo;
+    );
+  }
+}
