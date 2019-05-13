bfa94e5caa490ab1d2e5d52e34e6d64eec850076
Victor G <victorgordian103@gmail.com>
2019-05-13 15:05:20 -0700

    upload application is styled and shows with state

--- front-end/src/components/AddTenant/houseApp.js
+++ front-end/src/components/AddTenant/houseApp.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Dropzone from "react-dropzone";
 import axios from "axios";
 
 const url = "https://tenantly-back.herokuapp.com/images";
@@ -7,39 +8,39 @@ class HouseApp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      file: null,
-      url: null
+      imgSrc: null
     };
-    this.onChange = this.onChange.bind(this);
   }
-
-  onChange(e) {
-    this.setState({ file: e.target.files[0] });
-    const formData = new FormData();
-    formData.append("image", this.state.file);
-    const config = {
-      headers: {
-        "content-type": "multipart/form-data"
-      }
-    };
-    axios
-      .post(url, formData, config)
-      .then(response => {
-        this.setState({ url: response.data });
-        this.updateUrl();
-      })
-      .catch(error => {});
-  }
-
-  updateUrl() {
-    this.props.url(this.state.url);
-  }
+  handleOnDrop = file => {
+    if (file && file.length > 0) {
+      this.setState({ imgSrc: file[0].name });
+    }
+  };
 
   render() {
     return (
       <div className="addApp">
-        <h2>Housing Application</h2>
-        <input type="file" name="image" onChange={this.onChange} />
+        <h1>Housing Application</h1>
+        {this.state.imgSrc !== null ? (
+          <div
+            onClick={() => {
+              this.setState({ imgSrc: null });
+            }}
+          >
+            <h1>{this.state.imgSrc}</h1>{" "}
+          </div>
+        ) : (
+          <Dropzone onDrop={this.handleOnDrop}>
+            {({ getRootProps, getInputProps }) => (
+              <div>
+                <div {...getRootProps()} className="dropzone">
+                  <input {...getInputProps()} />
+                  <p>Upload Application</p>
+                </div>
+              </div>
+            )}
+          </Dropzone>
+        )}
       </div>
     );
   }
