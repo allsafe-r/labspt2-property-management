833640549b98bd51a47553be068e0ca3f3ff2fc3
Victor G <victorgordian103@gmail.com>
2019-05-13 17:29:37 -0700

    cloudinary works

--- front-end/src/components/AddTenant/houseApp.js
+++ front-end/src/components/AddTenant/houseApp.js
@@ -8,14 +8,34 @@ class HouseApp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      imgSrc: null
+      imgSrc: null,
+      file: null,
+      url: null
     };
   }
   handleOnDrop = file => {
     if (file && file.length > 0) {
-      this.setState({ imgSrc: file[0].name });
+      this.setState({ imgSrc: file[0].name, file: file[0] });
+      const formData = new FormData();
+      formData.append("image", this.state.file);
+      const config = {
+        headers: {
+          "content-type": "multipart/form-data"
+        }
+      };
+      axios
+        .post(url, formData, config)
+        .then(response => {
+          this.setState({ url: response.data });
+          this.updateUrl();
+        })
+        .catch(error => {});
     }
   };
+
+  updateUrl() {
+    this.props.url(this.state.url);
+  }
 
   render() {
     return (
