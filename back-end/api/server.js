28c64584e916184977a4544765b7bd67e3f51507
unknown <kyleteeter12@gmail.com>
2019-05-16 22:24:59 -0500

    Update changes

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
-const users = require("./users.js");
-const properties = require("./properties.js");
-const workOrders = require("./workorders.js");
-const stripe = require("./stripe.js");
-const images = require("./images.js");
-const login = require("./login.js");
-const billing = require("./billinghistory.js");
-const errorHandler = require("../errorHandler/errors.js");
-const mail = require("./nodemailer.js");
-=======
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
@@ -25,25 +11,12 @@ const authentication = require('./authentication.js');
 const billing = require('./billing.js');
 const errorHandler = require('../errorHandler/errors.js');
 const mail = require('./nodemailer.js');
->>>>>>> 5f44f16e656c73df5587b242fc35d5c00a1b60c7
 const server = express();
 const alerts = require("./alerts");
 const contracts = require("./contracts.js");
 
 server.use(express.json(), cors(), helmet());
 server.use(errorHandler);
-<<<<<<< HEAD
-server.use("/users", users);
-server.use("/workorders", workOrders);
-server.use("/properties", properties);
-server.use("/stripe", stripe);
-server.use("/api", login);
-server.use("/images", images);
-server.use("/alerts", alerts);
-server.use("/billing", billing);
-server.use("/send", mail);
-server.use("/contracts", contracts);
-=======
 server.use('/tenants', tenants);
 server.use('/landlords', landlords);
 server.use('/workorders', workOrders);
@@ -54,7 +27,6 @@ server.use('/images', images);
 server.use('/alerts', alerts);
 server.use('/billing', billing);
 server.use('/send', mail);
->>>>>>> 5f44f16e656c73df5587b242fc35d5c00a1b60c7
 
 server.get("/", (req, res) => {
   res.status(200).send("Hi");
